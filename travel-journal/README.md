48914be0ffc8b64ec2b204a2dc5ee04faa6313fa
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-03-08 19:27:38 +0530

    Pass props to Entry component for dynamic data rendering

--- travel-journal/README.md
+++ travel-journal/README.md
@@ -20,5 +20,16 @@
    that goes next to the location name
  – The main purpose of this challenge is to show you where our limitations*   currently are, so don't worry about the fact that you're hard-coding all
    this data into the component.
+
+## Passing Props in the Entry component 
+ Challenge: pass props to the Entry component to display
+ its data. See the `data.md` file for each prop name and its
+ value.
+ 
+ Then on the Entry component, receive and display the values
+ for those props. In the end, the page should look the same
+ as it does now, but without all the hard-coded data in the
+ component
+ 
  
  
--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -1,23 +1,20 @@
 
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="cover-image">
-                <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji" />
+                <img src={props.img.src} alt={props.img.alt} />
             </div>
             <div className="main-content">
                 <div className="top-content">
                     <i class="fa-solid fa-location-dot"></i>
-                    <span className="country-name">JAPAN</span>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu
-" className="google-link" target="_blank">View on Google Maps</a>
-                    <h2 className="location-name">Mount Fuji</h2>
+                    <span className="country-name">{props.country}</span>
+                    <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
+                    <h2 className="location-name">{props.title}</h2>
                 </div>
                 <div className="bottom-content">
-                    <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
-                    <p className="desc">
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                    </p>
+                    <p className="date">{props.date}</p>
+                    <p className="desc">{props.text}</p>
                 </div>
             </div>
         </article>
