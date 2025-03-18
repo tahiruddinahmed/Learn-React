2d52846d875b8e2b133af9b505f5bd301c8dfd6a
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-03-18 21:31:36 +0530

    Refector form submission

--- chef-cloude/src/components/Main.jsx
+++ chef-cloude/src/components/Main.jsx
@@ -1,33 +1,25 @@
 import React, {useState} from "react";
 export default function Main() {
 
-    /**
-     * Challenge: Update our app so that when the user enters a
-     * new ingredient and submits the form, it adds that new
-     * ingredient to our list!
-     */
-
     const [stateIngredients, setIngredients] = useState([])
-
-    // const ingrediants = ['Mango', 'Orange', 'Banana'];
 
     const IngredientsListItems = stateIngredients.map((item) => (
         <li key={item}>{item}</li>
     ));
 
+    /**
+     * Challenge: use form action instead of onSubmit to
+     * handle the data from the form
+     */
 
-    function handleForm(e) {
-        e.preventDefault();
-        // console.log("Form is submitted");
-
-        const formData = new FormData(e.currentTarget);
+    function addIngredients(formData) {
         const newIngredient = formData.get("ingredient");
         setIngredients((prevIngredient) => [...prevIngredient, newIngredient])
     }
 
     return (
         <main className="main">
-            <form onSubmit={handleForm}>
+            <form action={addIngredients}>
                 <input 
                     type="text" 
                     aria-label="add ingrediant" 
