66bab2cea1c683812c4d782c24c0272b3c557a1e
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-03-19 21:47:23 +0530

    Learn various form inputs, how to get the data from various form input

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,42 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  // function handleForm(event) {
-  //   event.preventDefault();
-  //   const formEl = event.currentTarget;
-  //   const formData = new FormData(formEl);
-  //   const email = formData.get("email");
-  //   const password = formData.get("password");
-
-  //   console.log(email, password);
-
-  //   formEl.reset()
-  // }
-
   function signUp(formData) {
     const email = formData.get("email");
     const password = formData.get("password");
-
-    console.log(email, password);
+    const desc = formData.get("desc");
+    const employementStatus = formData.get("employementStatus");
+    const dietaryRestrictions = formData.getAll("dietaryRestrictions");
+    const favColor = formData.get("favColor");
+    console.log(email, password, desc, employementStatus, dietaryRestrictions, favColor);
   }
 
   return (
     <section>
-      <h1 className='heading'>Sign Up Form</h1>
+      <h1 className="heading">Sign Up Form</h1>
       <form action={signUp}>
         <label htmlFor="email">Email</label>
-        <input id='email' type="email" name='email'/>
+        <input
+          id="email"
+          defaultValue="01tahirahmed@gmail.com"
+          type="email"
+          name="email"
+        />
 
         <label htmlFor="password">Password</label>
-        <input id='password' type="password" name='password' />
+        <input
+          id="password"
+          defaultValue="tahir@123"
+          type="password"
+          name="password"
+        />
+
+        <label htmlFor="desc">Description</label>
+        <textarea
+          name="desc"
+          id="desc"
+          defaultValue="This is a description"
+        ></textarea>
+
+        <fieldset>
+          <legend>Employement Status</legend>
+          <div>
+            <input
+              type="radio"
+              id="unemployement"
+              name="employementStatus"
+              value="unemployed"
+            />
+            <label htmlFor="unemployement">Unemployed</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="part-time"
+              name="employementStatus"
+              value="part-time"
+            />
+            <label htmlFor="part-time">Part-Time</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="full-time"
+              name="employementStatus"
+              defaultChecked={true}
+              value="full-time"
+            />
+            <label htmlFor="full-time">Full-Time</label>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Dietary Restrictions</legend>
+          <div>
+            <input
+              type="checkbox"
+              id="koshar"
+              name="dietaryRestrictions"
+              value="koshar"
+            />
+            <label htmlFor="koshar">Koshar</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="vegan"
+              name="dietaryRestrictions"
+              value="vegan"
+            />
+            <label htmlFor="vegan">Vegan</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="glutan-free"
+              name="dietaryRestrictions"
+              defaultChecked={true}
+              value="glutan-free"
+            />
+            <label htmlFor="glutan-free">Glutan-free</label>
+          </div>
+        </fieldset>
+
+        <label htmlFor="favColor">What is your favorite Color?</label>
+        <select name="favColor" id="favColor" defaultValue="">
+          <option value="">-- Choose a Color --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="pink">Pink</option>
+          <option value="violet">Violet</option>
+        </select>
 
         <button>Submit</button>
       </form>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
